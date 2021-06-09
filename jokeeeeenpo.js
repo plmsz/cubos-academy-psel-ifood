298fa5c8d9cc2fd9433f216ff5929355243cdea8
Paloma <plmsouzaoliveira@gmail.com>
2021-06-09 00:03:24 -0300

    Adiciona desafio Jokeeeeenpo

---
+++ jokeeeeenpo.js
@@ -0,0 +1,17 @@
+function solucao(joao, andre) {
+    if (joao === andre){
+        console.log("EMPATE")
+    }else if(joao === "PEDRA" && andre === "TESOURA"){
+        console.log("JOAO")
+    }else if(joao === "TESOURA" && andre === "PAPEL"){
+        console.log("JOAO")
+    }else if(joao === "PAPEL" && andre === "PEDRA"){
+        console.log("JOAO")
+    }else if(andre === "PEDRA" && joao === "TESOURA"){
+        console.log("ANDRE")
+    }else if(andre === "TESOURA" && joao === "PAPEL"){
+        console.log("ANDRE")
+    }else if(andre === "PAPEL" && joao === "PEDRA"){
+        console.log("ANDRE")  
+    }
+}
